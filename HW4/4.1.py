344ae3951ce450d99768bf54b11e86fcbb625d31
Vitalii <sv301185@gmail.com>
2020-07-17 15:14:40 +0300

    New HomeWork

---
+++ HW4/4.1.py
@@ -0,0 +1,7 @@
+text = '''Beautiful is better than ugly\n\
+Explicit is better than implicit\n\
+Simple is better than complex\n\
+Complex is better than complicated\n\
+Readability counts\n'''
+
+print(text.lower(), text.upper(), sep = '\n')
---
+++ HW4/4.2.py
@@ -0,0 +1,4 @@
+number = input('Give 4-gigits number:')
+num = int(number[0]) * int(number[1]) * int(number[2]) * int(number[3])
+invers = number[::-1]
+print('a) %d\nb) %s' %(num, invers))
