39e68116c7fa9732e8c91d55addbd1ac5f6de6ed
Vitalii <sv301185@gmail.com>
2020-07-22 21:14:55 +0300

    Changed task 5

--- HW5/5.5.1.py
+++ HW5/5.5.1.py
@@ -6,18 +6,24 @@ while h < p:
 sumLesP = 0
 multGrateH = 1
 count = 0
-j = int(input("Your number: "))
-while j != p and j != h:
+
+def check(p, h):
+    global j
+    j = int(input("Number "))
+    return j != p and j != h
+
+while check(p ,h):
     if j < p:
         sumLesP += j
     elif j > h:
         multGrateH *= j
     else:
         count += 1
-    j = int(input("Your number: "))
+
 if multGrateH == 1:
    multGrateH = 0 
 print("""Cума чисел, які менше за 'Star of range' = %d\n
 Добуток чисел, які більші за 'End of range' = %d\n
 Кількість чисел, що знаходяться  в діапазоні значень від 'Start' до 'End' = %d"""\
       %(sumLesP, multGrateH, count))
+
