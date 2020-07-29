41f6ab4ea0c449f1058ffcb80acabe3282125ad0
Vitalii <sv301185@gmail.com>
2020-07-29 22:00:46 +0300

    Delete debuging line from 7 task

--- HW7/7.7.py
+++ HW7/7.7.py
@@ -6,11 +6,9 @@
 def date(day, month, year):
     '''Recognize if date is real'''
     if month in [1, 3, 5, 7, 8, 10, 12]:
-        print(1)
         if 0 < day < 32: return True
         else: return False
     elif month == 2:
-        print(2)
         if year % 400 == 0 and year % 100 == 0 and year % 4 == 0:
             if 0 < day < 30: return True
             else: return False
@@ -19,7 +17,6 @@ def date(day, month, year):
             else: return False
         else: return False
     elif month in [4, 6, 9, 11]:
-        print(3)
         if 0 < day < 31: return True
         else: return False
     else: return False
